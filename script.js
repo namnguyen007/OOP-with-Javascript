333a80ca988ac3ae7fae0c20814b4176ae9cbb04
Nam Nguyen <nguyenthanhnamjuly27th1990@gmail.com>
2021-02-25 19:04:54 +0700

    prototype in JS

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 'use strict';
-  const Person = function(firstName, birthYear) {
-   // Instance properties
-   this.firstName = firstName;
-   this.birthYear = birthYear;
-   // Instance method (this is very bad practice)
-   // imagine 1000 instances copying the same method like this everywhere
-   this.calcAge = function() {
-    console.log(2037 - this.birthYear);
-   }
-  }
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
 
-  const jonas = new Person('Jonas', 1990);
-  // console.log(jonas);
-  const matilda = new Person('Matilda', 2017);
-  const jack = new Person('Jack', 1975);
-  // console.log(jack instanceof Person);
+const jonas = new Person('Jonas', 1990);
+// console.log(jonas);
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+// console.log(jack instanceof Person);
+// console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+jonas.calcAge()
+console.log(Person.prototype);
+console.log(jonas.__proto__);
+console.log(Person.prototype.isPrototypeOf(jonas));
