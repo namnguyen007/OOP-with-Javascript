1552aff253c3001855a21b91e0fa1a5a4ffdcc5d
Nam Nguyen <nguyenthanhnamjuly27th1990@gmail.com>
2021-02-25 18:32:34 +0700

    Class(constructor func in JS) - instances

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+  const Person = function(firstName, birthYear) {
+   // Instance properties
+   this.firstName = firstName;
+   this.birthYear = birthYear;
+   // Instance method (this is very bad practice)
+   // imagine 1000 instances copying the same method like this everywhere
+   this.calcAge = function() {
+    console.log(2037 - this.birthYear);
+   }
+  }
+
+  const jonas = new Person('Jonas', 1990);
+  // console.log(jonas);
+  const matilda = new Person('Matilda', 2017);
+  const jack = new Person('Jack', 1975);
+  // console.log(jack instanceof Person);
